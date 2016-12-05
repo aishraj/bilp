6b18e17d0e55b4710f67ffb58ab05d1aef48c148
Aish Raj Dahal <aish@aishraj.com>
2016-12-05 06:46:22 -0800

    Minor cleanup

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-while ! (bash ./wait-for-it.sh db:5432); do sleep 4; done
-echo "Starting the program";
+while ! (bash ./wait-for-it.sh db:5432); do sleep 1; done
+echo "Starting the server";
 python botme.py
